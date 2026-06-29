ae918b21f5b8cbebe9e1491642798499a98e4362
Sara Valentina Leon Hernandez <120668241+SaraLeon22@users.noreply.github.com>
2026-06-28 22:11:40 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+Instagram.addEventListener("click", () => {
+
+    window.open("https://www.instagram.com/lavadisenosoluciones/", "_blank");
+});
+
+TikTok.addEventListener("click", () => {
+
+    window.open("https://www.tiktok.com/@lavadisenosoluciones", "_blank");
+});
