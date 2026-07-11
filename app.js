735cf88f263a6caafea41fcfffa53b8b220ea85c
Sara Valentina Leon Hernandez <120668241+SaraLeon22@users.noreply.github.com>
2026-07-11 17:56:47 -0500

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-Instagram.addEventListener("click", () => {
-
-    window.open("https://www.instagram.com/lavadisenosoluciones/", "_blank");
-});
-
-TikTok.addEventListener("click", () => {
-
-    window.open("https://www.tiktok.com/@lavadisenosoluciones", "_blank");
-});
